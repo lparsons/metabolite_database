cdcd20a8d93998d0b5f2541d90827dedda37c5b3
Lance Parsons <lparsons@princeton.edu>
2018-06-27 15:14:12 -0400

    Added tests, dotenv, and requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Rabinowitz Lab Metabolite Database
 
 Metabolite database used by the Rabinowitz Lab
+
+
+# Configuration
+
+1.  Copy `env.sample` to `.env` and edit as needed
+2.  `export FLASK_APP=main.py`
+3.  `export FLASK_ENV=development`
+4.  `flask run`
---
+++ tests.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+from datetime import datetime
+import unittest
+from config import Config
+from metabolite_database import create_app
+from metabolite_database import db
+from metabolite_database.models import Compound
+from metabolite_database.models import ChromatographyMethod
+from metabolite_database.models import RetentionTime
+from metabolite_database.models import StandardRun
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class CompoundModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_compound(self):
+        c = Compound(name="Test Compound",
+                     molecular_weight=12,
+                     molecular_formula="C2O6")
+        self.assertEqual(c.name, "Test Compound")
+        self.assertEqual(c.molecular_weight, 12)
+        self.assertEqual(c.molecular_formula, "C2O6")
+
+    def test_retention_time(self):
+        c = Compound(name="Test Compound",
+                     molecular_weight=12,
+                     molecular_formula="C2O6")
+        cm = ChromatographyMethod(name="Test Method")
+        rt = RetentionTime(retention_time=5.25)
+        sr = StandardRun(date=datetime.utcnow(), operator="Lance")
+        c.retention_times.append(rt)
+        rt.standard_run = sr
+        sr.chromatography_method = cm
+        db.session.add_all([c, cm, rt, sr])
+        db.session.commit()
+        res = cm.compounds_with_retention_times()
+        res_list = list(res)
+        self.assertEqual(len(res_list), 1)
+        self.assertEqual(res_list[0], (c, rt))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
